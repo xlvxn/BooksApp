97776983bec620dc1cfa3d82250e5e6937e00437
xlvxn <alvin.fauzi.aulia@gmail.com>
2021-12-27 15:32:15 +0800

    refactoring update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,30 +36,25 @@ function main() {
         }
     }
 
-    const updateBook = (book) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
+    const updateBook = async (book) => {
+        try {
+            const options = {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Auth-Token": "12345"
+                },
+                body: JSON.stringify(book)
+            }
 
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function () {
-            const responseJson = JSON.parse(this.responseText);
+            const response = await fetch(`${baseUrl}/edit/${book.id}`, options);
+            const responseJson = await response.json();
+
             showResponseMessage(responseJson.message);
             getBook();
+        } catch (error) {
+            showResponseMessage(error);
         }
-
-        xhr.onerror = function () {
-            showResponseMessage();
-        }
-
-        // Membuat PUT request dan menetapkan target URL
-        xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
-
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
     };
 
     const removeBook = (bookId) => {
